23f4a5abb92ff95c42ce39e3f96624ccef09fa09
Antonio Rizerio <antonio.rizerio@gmail.com>
2022-02-18 18:30:19 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -0,0 +1,94 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/** 
+ *  Quando um objeto for transformado em uma cadeia de bytes, para que o objeto possa trafegar na rede, para que o objeto
+ *  possa ser gravado em arquivos... Precisa da interface Serializable implementada.
+ *
+ */
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Long id;
+	private String name;
+    private String email;
+    private String phone;
+    private String password;
+    
+    // Vazio, pois estou utilizando frameworks //
+    public User() {
+    	
+    }
+
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+    
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, name, password, phone);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(password, other.password) && Objects.equals(phone, other.phone);
+	}
+
+	// getters and setters //
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educandoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.educandoweb.course.entities.User;
+
+@RestController // Recurso WEB que é implementado por um Controlador REST //
+@RequestMapping(value = "/users") 
+public class UserResource {
+	
+	// ENDPOINT para acessar os usuários //
+	@GetMapping   // Responde requisição do tipo GET do HTTP //
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "992512006", "kaka22");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
