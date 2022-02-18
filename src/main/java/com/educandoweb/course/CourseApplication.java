20e27abaf44494b38cc93c5d15f1f15e7f634ae7
Antonio Rizerio <antonio.rizerio@gmail.com>
2022-02-18 17:20:56 -0300

    Project created

---
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -0,0 +1,14 @@
+package com.educandoweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		// Primeiro Comando para minha aplicação Spring Boot Rodar //
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
